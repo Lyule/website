68a1a48f7b6a0c04752a0eaec81f8a06d791faaf
[lyule] <[857277886@qq.com]>
2018-04-11 14:04:51 +0800

    login interface

--- apps/router.js
+++ apps/router.js
@@ -1,5 +1,39 @@
 module.exports = app => {
-	app.get('/', (req, res, err) => {
-		return res.render('index');
-	});
+    app.get('/', (req, res, err) => {
+        return res.render('index');
+    });
+
+    app.get('/login', (req, res, err) => {
+
+        if (!req.query.username || !req.query.password) {
+            return res.send({
+                code: -1,
+                data: req.query,
+                msg: 'username或者password输入不正确'
+            });
+        }
+
+        return res.send({
+            code: 1,
+            data: req.query,
+            msg: 'success'
+        });
+    });
+
+
+    app.post('/login', (req, res, next) => {
+    	if(!req.body.username || !req.body.password){
+    		return res.send({
+                code: -1,
+                data: req.body,
+                msg: 'username或者password输入不正确'
+            });
+    	}
+
+    	return res.send({
+            code: 1,
+            data: req.body,
+            msg: 'success'
+        });
+    });
 }
